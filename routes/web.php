e03d60847b749f99ac93426523f82e4e50650162
aymeric70 <noahaymeric@gmail.com>
2019-11-27 09:44:38 +0100

    configure database

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/services/all', 'ServiceController@all')->name('all_services');
